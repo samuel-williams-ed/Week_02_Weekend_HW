7a93d27f9306cc06408dbcd660ff0518d1675d8b
Samuel Williams <samuel.williams.ed@gmail.com>
2022-12-02 11:48:53 +0000

    Defined Guest, Song & Room classes

---
+++ src/rooms.py
@@ -0,0 +1,9 @@
+class Room():
+    def __init__(self, input_number, input_capacity, input_price):
+        self.number = input_number
+        self.capacity = input_capacity
+        self.price = input_price
+        self.songs_list = []
+    
+    # Methods:
+    
---
+++ src/songs.py
@@ -0,0 +1,8 @@
+class Song():
+    def __init__(self, input_name, input_artist, input_genre):
+        self.name = input_name
+        self.artist = input_artist
+        self.genre = input_genre
+    
+    # Methods:
+    
