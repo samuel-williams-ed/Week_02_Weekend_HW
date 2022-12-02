82cc364f3337113cd1162f27a119d4ff09f53b75
Samuel Williams <samuel.williams.ed@gmail.com>
2022-12-02 12:51:56 +0000

    Added func stop rooms being checked into when full

--- src/rooms.py
+++ src/rooms.py
@@ -3,14 +3,25 @@ class Room():
         self.number = input_number
         self.capacity = input_capacity
         self.price = input_price
+        self.guests_list = []
         self.songs_list = []
     
     # Methods:
     def return_if_customer_is_checked_in(self, customer_object): #might make more sense to make this a guest method
         return customer_object.checked_in #False value by default
 
+    def check_room_has_space(self):
+        if self.capacity > len(self.guests_list):
+            return True
+        return False
+
     def check_in_guest(self, customer_object):
-        customer_object.checked_in = True
+        #add test to check if there is space in the room
+        if self.check_room_has_space():
+            customer_object.checked_in = True
+            self.guests_list.append(customer_object)
+        else: 
+            print("This room is full!")
     
     def check_out_guest(self, customer_object):
         customer_object.checked_in = False
--- tests/room_test.py
+++ tests/room_test.py
@@ -6,13 +6,13 @@ from src.songs import Song
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
-        self.test_room = Room(7, 17, 23.00)
+        self.test_room = Room(7, 2, 23.00)
         self.test_guest = Guest("Herbert Farthingsworth", 38)
         self.test_song = Song("Number 9", "Moon Hooch", "Dirty Funk/Dance Fusion Jazz")
 
     def test_room_was_created(self):
         self.assertEqual(7, self.test_room.number)
-        self.assertEqual(17, self.test_room.capacity)
+        self.assertEqual(2, self.test_room.capacity)
         self.assertEqual(23.00, self.test_room.price)
 
     def test_guest_was_checked_in(self):
@@ -27,4 +27,11 @@ class TestRoom(unittest.TestCase):
     def test_song_was_added_to_room(self):
         self.test_room.add_song_to_room(self.test_song)
         self.assertEqual([self.test_song], self.test_room.songs_list)
+
+    #Extensions:
+    def test_customer_cant_check_into_full_room(self):
+        self.test_room.check_in_guest(self.test_guest)
+        self.test_room.check_in_guest(self.test_guest)
+        self.assertEqual(False, self.test_room.check_room_has_space())
+
         
